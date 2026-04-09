3247c341cfcb031bbf685c2fdc2806fc2271a419
Felicià MAVIANE <maviane@gmail.com>
2026-04-09 17:13:05 +0200

    Formatted files

--- enderscope/enderlights_pi.py
+++ enderscope/enderlights_pi.py
@@ -7,14 +7,16 @@ import neopixel
 PIN = board.D18
 LED_COUNT = 16
 
-class CardPoint(Enum):
-    NORTH = (0,3)
-    EAST = (4,7)
-    SOUTH = (8,11) 
-    WEST = (12,15)
 
-class Enderlights():
-    def __init__(self, pin=PIN, led_count:int=LED_COUNT):
+class CardPoint(Enum):
+    NORTH = (0, 3)
+    EAST = (4, 7)
+    SOUTH = (8, 11)
+    WEST = (12, 15)
+
+
+class Enderlights:
+    def __init__(self, pin=PIN, led_count: int = LED_COUNT):
         self.pixels = neopixel.NeoPixel(PIN, led_count)
         self.led_count = led_count
 
@@ -24,17 +26,17 @@ class Enderlights():
     def __setitem__(self, index, value):
         self.pixels[index] = value
 
-    def fill(self, value:list|tuple)->None:
+    def fill(self, value: list | tuple) -> None:
         self.pixels.fill(value)
 
-    def set_slice(self, start:int, end:int, value:tuple):
+    def set_slice(self, start: int, end: int, value: tuple):
         self[start:end] = (end - start) * [value]
 
-    def shutter(self, state:bool, value:list|tuple=(255,255,255))->None:
+    def shutter(self, state: bool, value: list | tuple = (255, 255, 255)) -> None:
         if state is True:
             self.fill(value)
         else:
-            self.fill((0,0,0))
+            self.fill((0, 0, 0))
 
     def red(self, value):
         for i in range(self.led_count):
@@ -46,16 +48,22 @@ class Enderlights():
 
     def blue(self, value):
         for i in range(self.led_count):
-            self[i] = (self[i][0], self[i][0],value)
+            self[i] = (self[i][0], self[i][0], value)
 
-    def set_cardinal(self, card_point:Literal[CardPoint.NORTH, CardPoint.SOUTH, CardPoint.EAST,CardPoint.WEST], value:tuple):
+    def set_cardinal(
+        self,
+        card_point: Literal[
+            CardPoint.NORTH, CardPoint.SOUTH, CardPoint.EAST, CardPoint.WEST
+        ],
+        value: tuple,
+    ):
         start, end = card_point.value
-        self[start:end+1]=(end-start+1)*[value]
-        
+        self[start : end + 1] = (end - start + 1) * [value]
 
-def cycle(lights, size, step, value=(255,255,255)):
+
+def cycle(lights, size, step, value=(255, 255, 255)):
     for i in range(0, lights.led_count, step):
         lights.shutter(False)
-        start, stop = i, min(i+size, lights.led_count)
+        start, stop = i, min(i + size, lights.led_count)
         lights[start:stop] = size * [value]
         yield start, stop
